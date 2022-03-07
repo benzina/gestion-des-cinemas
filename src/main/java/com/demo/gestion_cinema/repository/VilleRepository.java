e3ba005224a4bd67b859007bc8fc6598394353c6
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-03-07 16:50:20 +0100

    FEATURE/PO-I3: repository layer updating

--- src/main/java/com/demo/gestion_cinema/repository/VilleRepository.java
+++ src/main/java/com/demo/gestion_cinema/repository/VilleRepository.java
@@ -4,4 +4,5 @@ import com.demo.gestion_cinema.entity.Ville;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VilleRepository extends JpaRepository<Ville,Long> {
+    Ville findByName(String name);
 }
